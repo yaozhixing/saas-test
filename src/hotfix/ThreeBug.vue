b5c8d0d42b640f1d0a35d3fa639fc67afdf34854
yaozhixing <yaozhixing797600@163.com>
2021-02-02 09:54:24 +0800

    hotfix fix three bug

---
+++ src/hotfix/ThreeBug.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>fix bug</h2>
+    <div>fix second bug!!!</div>
+    <div>fix three bug!!!</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: bug
+}
+</script>
+
+<style>
+</style>
